7526c7adc0826a5e1f2b25ae89494da77261e081
Joydeep Sur <joydeepsur6@gmail.com>
2020-03-31 14:45:52 +0530

    initial commit

---
+++ src/main/java/com/example/dev/SpringBootDevToolsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDevToolsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDevToolsExampleApplication.class, args);
+	}
+
+}
